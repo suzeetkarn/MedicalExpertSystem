944e71889754ebb81027f1851fe3f1344e27aab9
suzeetkarn <34273202+suzeetkarn@users.noreply.github.com>
2022-05-04 18:41:37 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,61 @@
 package com.mycompany.app;
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class App
 {
+    @SuppressWarnings("unchecked")
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Enter your symptoms : ");
+        String str= sc.nextLine();
+        String[] words = str.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+                // You may want to check for a non-word character before blindly
+                // performing a replacement
+                // It may also be necessary to adjust the character class
+                words[i] = words[i].replaceAll("[^\\w]", "");
+        }
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("/src/assets/data.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray covidReport = (JSONArray) obj;
+            int size = covidReport.size();
+            Object[] stringArray = covidReport.toArray(new String[size]);
+
+            for(int i=0;i<words.length;i++)
+            {
+                for(int j=0;j<size ;j++){
+                    if(words[i] == stringArray[j]){
+                        System.out.println("you are at Covid Risk");
+
+                    }
+                    else{
+                        System.out.println("You are covid Risk free");
+
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
